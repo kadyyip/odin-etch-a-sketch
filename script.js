5b3f4da2e95d34ea7668cd3bf6da49d89817ed71
Kady Yip <kadyy1998@gmail.com>
2023-09-13 22:30:44 -0400

    Create grid

---
+++ script.js
@@ -0,0 +1,18 @@
+function createGrid(numSquaresPerSide) {
+    const grid = document.querySelector(".grid");
+    let totalSquares = numSquaresPerSide ** 2
+    let borderPx = 0.5;
+    let squareLength = (grid.offsetWidth / numSquaresPerSide) - borderPx * 2;
+    
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        console.log(`background-color: blue; width: ${squareLength}px; 
+        height: ${squareLength}px; border: black solid ${borderPx}px;`)
+        square.style.cssText = `width: ${squareLength}px; 
+            height: ${squareLength}px; border: #a6a6a6 solid ${borderPx}px;`; 
+        grid.appendChild(square);
+    }
+}
+
+createGrid(16);
