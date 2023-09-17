e3c52988f59636dd34642e2a1edd1cb7c14959bd
Kady Yip <kadyy1998@gmail.com>
2023-09-16 21:35:51 -0400

    Add color modes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-etch-a-sketch
+
+Created as part of The Odin Project, this is a page that allows you to fill in the grid as you hover over it.
+
+This project display HTML, CSS, and Javascript skills, in particular DOM manipulation.
--- script.js
+++ script.js
@@ -14,13 +14,46 @@ function createGrid(numSquaresPerSide) {
     }
 }
 
+function generateRandomColor() {
+    // generate RGB value
+    let r = randomInt(257);
+    let g = randomInt(257);
+    let b = randomInt(257);
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function fillSquare(event) {
-    event.target.classList.add("fill");
+    if (colorMode === "pick") {
+        const colorPicker = document.querySelector("#color-picker");
+        let selectedColor = colorPicker.value;
+        event.target.style.backgroundColor = selectedColor;
+    } else if (colorMode === "rainbow") {
+        const rainbow = ["#FAEDCB", "#C9E4DE", "#C6DEF1", 
+            "#DBCDF0", "#F2C6DE", "#F7D9C4"];
+        let color = rainbow[randomInt(7)];
+        event.target.style.backgroundColor = color;
+    } else if (colorMode === "random") {
+        let color = generateRandomColor();
+        event.target.style.backgroundColor = color;
+    } else if (colorMode === "eraser") {
+        event.target.style.backgroundColor = "";
+    } else return;
 }
 
 function removeGrid() {
     const grid = document.querySelector(".grid");
     grid.replaceChildren();
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll(".square");
+    for (const square of squares) {
+        square.style.backgroundColor = "";
+    }
 }
 
 function updateGridSize(event) {
@@ -34,10 +67,36 @@ function updateGrid(event) {
     createGrid(numSquaresPerSide);
 }
 
+function updateColorMode(event) {
+    const active = document.querySelector(".active");
+    active.classList.remove("active");
+    console.log(event.target.id);
+    if (event.target.id === "color-picker") {
+        console.log(event.target.parentNode);
+        colorMode = event.target.parentNode.id;
+        event.target.parentNode.classList.add("active");
+    } else {
+        colorMode = event.target.id;
+        event.target.classList.add("active");
+    }
+}
+
 const slider = document.querySelector(".slider");
 const gridSize = document.querySelector(".grid-size");
 gridSize.textContent = `Grid Size: ${slider.value} x ${slider.value}`;
 slider.addEventListener("change", updateGrid);
 slider.addEventListener("input", updateGridSize);
 
+const pick = document.querySelector("#pick");
+const random = document.querySelector("#random");
+const rainbow = document.querySelector("#rainbow");
+const eraser = document.querySelector("#eraser");
+const clear = document.querySelector("#clear");
+pick.addEventListener("click", updateColorMode);
+random.addEventListener("click", updateColorMode);
+rainbow.addEventListener("click", updateColorMode);
+eraser.addEventListener("click", updateColorMode);
+clear.addEventListener("click", clearGrid);
+
+let colorMode = "pick";
 createGrid(slider.value);
